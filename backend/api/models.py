e7b2a3d940c5f00cdf827a61e5dd01424a83d985
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-12 19:11:48 +0000

    setting up the models for the backend db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create your models here. model create db tables and feilds programmatically
+
+
+
+
+class User(AbstractUser): #extends abstract user that django already has setup for
+    name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
+    
+    USERNAME_FIELD ='email' # allows us to login with email instead of username
+    
