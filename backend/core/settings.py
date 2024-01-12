48460396de130ff1a36b19bc02e388240967d254
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-12 18:54:34 +0000

    setting up .env and secret keys

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+## Setting up .ENV
+from decouple import config
+
+DEBUG = config('DEBUG', cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hw=i*ox11i5g*u@!*z&y$@t@4v%^s_)=i%iwu%ovj-(fqfj_d%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
