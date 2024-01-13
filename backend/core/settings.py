7324c2ebb9a9e4c457d2c3412c81de42483d4ece
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-13 22:36:37 +0000

    Adding the allow credential to allow cookie

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'api',
 ]
 CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOW_CREDENTIALS = True
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -129,3 +131,4 @@ AUTH_USER_MODEL = 'api.User'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
