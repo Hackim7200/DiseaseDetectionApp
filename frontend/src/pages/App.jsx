b7669cba44757e31045b2baf23836f869a13efa7
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-14 21:20:54 +0000

    setting up the frontend for the project

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import "./App.css"
 import axios from "axios";
 
 function App() {
@@ -22,7 +21,7 @@ function App() {
 
   return (
     <>
-      <div></div>
+      <div>Hello</div>
     </>
   );
 }
