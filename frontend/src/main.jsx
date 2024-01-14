9fd59c46d46853958a9418918c39f4d00272d5a1
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-14 21:45:49 +0000

    setting up the router and pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import './index.css'
 
+import {createBrowserRouter, createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import HomeScreen from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+
+      <Route index={true} path='/' element={<HomeScreen/>}/>
+      <Route  path='/about' element={<About/>}/>
+      <Route  path='/about' element={<About/>}/>
+
+    </Route>
+  )
+)
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,29 +1,22 @@
-import { useEffect, useState } from "react";
-
-import "./App.css"
-import axios from "axios";
+// which ever page has the outlet comp becomes the layout design
+// import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+// import Hero from './components/Hero'
 
 function App() {
-
-  const getUser = async () => {
-    try {
-      console.log(await axios.get("http://127.0.0.1:8000/api/hello/"));
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(()=>{
-    getUser()
-
-  },[])
-
   return (
+      // where output is used you can use it to set the general design of the website e.g., header,footer
     <>
-      <div>Hello</div>
+    {/* <Header/>  */}
+    <div>
+      <Outlet/> 
+
+    </div>
     </>
-  );
+
+
+
+  )
 }
 
-export default App;
+export default App
