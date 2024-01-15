dbccec375bfa4ca43607864c915845438067f24d
Hikmatullah <hikmathakim7200@gmail.com>
2024-01-15 17:53:40 +0000

    Adding useEffects to monitor changes in inputs

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import {faCheck,faTimes,faInfoCircle} from "@fontawesome/free-solid-svg-icons"
+import {faCheck,faTimes,faInfoCircle} from "@fortawesome/free-solid-svg-icons"
 
 // regex from ->  https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
 
@@ -24,14 +24,52 @@ function Register() {
     const [userPwdMatchFocus,setPwdMatchFocus] = useState(false)
 
     const [errMsg,setErrMsg] = useState("")
-    const [successMsg,setSuccessMsg] = useState(False)
+    const [successMsg,setSuccessMsg] = useState(false)
+
+    useEffect(()=>{
+        setValidName(USER_REGEX.test(user))  //.test return true if matche else false
+
+    },[user])
+
+    useEffect(()=>{
+        setValidPwd(PWD_REGEX.test(pwd))  //.test return true if matche else false
+
+        if(pwd == pwdMatch)
+        {
+            setValidPwdMatch(true) 
+        } else setValidPwdMatch(false)
+    },[pwd,pwdMatch])
+
+    useEffect(()=>{
+        setErrMsg("") // reset the error msg anytime changes made
+
+    },[pwd,user,pwdMatch])
+
+
 
 
 
 
 
   return (
-    <div>Register</div>
+    <div>
+        
+        Register
+
+        <input value={user} type="text" onChange={(e)=>setUser(e.target.value)}/>
+        <label htmlFor="">{JSON.stringify(validName)}</label>
+
+        <input value={pwd} type="text" onChange={(e)=>setPwd(e.target.value)}/>
+        <label htmlFor="">{JSON.stringify(validPwd)}</label>
+
+        <input value={pwdMatch} type="text" onChange={(e)=>setPwdMatch(e.target.value)}/>
+        <label htmlFor="">{JSON.stringify(validPwdMatch)}</label>
+
+
+
+
+
+    </div>
   )
 }
 
