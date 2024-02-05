377be83bc7277f4f4d3635a3ea360dad7259e8a0
Hikmatullah <hikmathakim7200@gmail.com>
2024-02-05 19:43:45 +0000

    I have made the profile loging icon that appears when logged  and i have made the upload pop up window that allows you to upload images

---
+++ frontend/src/components/UploadImage/UploadImage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import './UploadImage.scss'
+const UploadImage = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return (
+    <div className="upload-component">
+      <h1>Upload and Display Image usign React Hook's</h1>
+
+      {selectedImage && (
+        <div>
+          <img
+            alt="not found"
+            width={"250px"}
+            src={URL.createObjectURL(selectedImage)}
+          />
+          <br />
+          <button onClick={() => setSelectedImage(null)}>Remove</button>
+        </div>
+      )}
+
+      <br />
+      <br />
+
+      <input
+        className="icon"
+        type="file"
+        name="myImage"
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+          setSelectedImage(event.target.files[0]);
+        }}
+      />
+    </div>
+  );
+};
+
+export default UploadImage;
--- frontend/src/pages/Detect/Detect.jsx
+++ frontend/src/pages/Detect/Detect.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import pic01 from "../../images/plantImg2.jpeg";
 import pic02 from "../../images/plantImg2.jpeg";
@@ -8,126 +8,138 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import PopUp from "../../components/PopUp/PopUp";
-
+import UploadImage from "../../components/UploadImage/UploadImage";
 
 function Detect() {
+  const pop = () => {
+    document.getElementById("overlay").style.display = "block";
+  };
+
   return (
-
     <>
-    <PopUp>
+      <PopUp title="Upload ">
+        <FontAwesomeIcon icon={faCamera} size="5x" />
+        <UploadImage />
+      </PopUp>
 
-      <button>hello</button>
-      <button>hello</button>
-      <button>hello</button>
-
-
-
-
-    </PopUp>
-
-
-				<div id="banner-wrapper">
-					<div id="banner" class="box container">
-						<div class="row">
-							<div class="col-7 col-12-medium">
-								<h2>Detect</h2>
-							</div>
-								<p>Here you can search previous detection history or detect new plants & diseases</p>
-					
-						</div>
-					</div>
-				</div>
-    
-    
-   
-    <div className="detectPageWrapper">
-      {/* <input className='searchBar' type="text" /> */}
-      <div className="search-box">
-        <div>
-          <input
-            name="search"
-            type="text"
-            placeholder="Jasminum officinale"
-            required
-          />
-          <span>
-            <FontAwesomeIcon icon={faCamera} size="4x" />
-          </span>
+      <div id="banner-wrapper">
+        <div id="banner" class="box container">
+          <div class="row">
+            <div class="col-7 col-12-medium">
+              <h2>Detect</h2>
+            </div>
+            <p>
+              Here you can search previous detection history or detect new
+              plants & diseases
+            </p>
+          </div>
         </div>
       </div>
 
-      {/* // <!-- Features --> */}
+      <div className="detectPageWrapper">
+        {/* <input className='searchBar' type="text" /> */}
+        <div className="search-box">
+          <div>
+            <input
+              name="search"
+              type="text"
+              placeholder="Jasminum officinale"
+              required
+            />
+            <span
+              onClick={() => {
+                pop();
+              }}
+            >
+              <FontAwesomeIcon icon={faCamera} size="4x" />
+            </span>
+          </div>
+        </div>
 
-      <div id="features-wrapper">
-        <div class="container">
-          <div class="row">
-            <div class="col-4 col-12-medium">
-              {/* <!-- Box --> */}
-              <Link style={{ all: "unset" }} to="/analytics" state={{ id: 0 }}>
-                <section class="box feature">
-                  <span class="image featured">
-                    <img src={pic01} alt="" />
-                  </span>
-                  <div class="inner">
-                    <header>
-                      <h2>Put something here</h2>
-                      <p>Maybe here as well I think</p>
-                    </header>
-                    <p>
-                      Phasellus quam turpis, feugiat sit amet in, hendrerit in
-                      lectus. Praesent sed semper amet bibendum tristique
-                      fringilla.
-                    </p>
-                  </div>
-                </section>
-              </Link>
-            </div>
-            <div class="col-4 col-12-medium">
-              {/* <!-- Box --> */}
-              <Link style={{ all: "unset" }} to="/analytics" state={{ id: 0 }}>
-                <section class="box feature">
-                  <span class="image featured">
-                    <img src={pic01} alt="" />
-                  </span>
-                  <div class="inner">
-                    <header>
-                      <h2>Put something here</h2>
-                      <p>Maybe here as well I think</p>
-                    </header>
-                    <p>
-                      Phasellus quam turpis, feugiat sit amet in, hendrerit in
-                      lectus. Praesent sed semper amet bibendum tristique
-                      fringilla.
-                    </p>
-                  </div>
-                </section>
-              </Link>
-            </div>
-            <div class="col-4 col-12-medium">
-              {/* <!-- Box --> */}
-              <Link style={{ all: "unset" }} to="/analytics" state={{ id: 0 }}>
-                <section class="box feature">
-                  <span class="image featured">
-                    <img src={pic01} alt="" />
-                  </span>
-                  <div class="inner">
-                    <header>
-                      <h2>Put something here</h2>
-                      <p>Maybe here as well I think</p>
-                    </header>
-                    <p>
-                      Phasellus quam turpis, feugiat sit amet in, hendrerit in
-                      lectus. Praesent sed semper amet bibendum tristique
-                      fringilla.
-                    </p>
-                  </div>
-                </section>
-              </Link>
+        {/* // <!-- Features --> */}
+
+        <div id="features-wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col-4 col-12-medium">
+                {/* <!-- Box --> */}
+                <Link
+                  style={{ all: "unset" }}
+                  to="/analytics"
+                  state={{ id: 0 }}
+                >
+                  <section class="box feature">
+                    <span class="image featured">
+                      <img src={pic01} alt="" />
+                    </span>
+                    <div class="inner">
+                      <header>
+                        <h2>Put something here</h2>
+                        <p>Maybe here as well I think</p>
+                      </header>
+                      <p>
+                        Phasellus quam turpis, feugiat sit amet in, hendrerit in
+                        lectus. Praesent sed semper amet bibendum tristique
+                        fringilla.
+                      </p>
+                    </div>
+                  </section>
+                </Link>
+              </div>
+              <div class="col-4 col-12-medium">
+                {/* <!-- Box --> */}
+                <Link
+                  style={{ all: "unset" }}
+                  to="/analytics"
+                  state={{ id: 0 }}
+                >
+                  <section class="box feature">
+                    <span class="image featured">
+                      <img src={pic01} alt="" />
+                    </span>
+                    <div class="inner">
+                      <header>
+                        <h2>Put something here</h2>
+                        <p>Maybe here as well I think</p>
+                      </header>
+                      <p>
+                        Phasellus quam turpis, feugiat sit amet in, hendrerit in
+                        lectus. Praesent sed semper amet bibendum tristique
+                        fringilla.
+                      </p>
+                    </div>
+                  </section>
+                </Link>
+              </div>
+              <div class="col-4 col-12-medium">
+                {/* <!-- Box --> */}
+                <Link
+                  style={{ all: "unset" }}
+                  to="/analytics"
+                  state={{ id: 0 }}
+                >
+                  <section class="box feature">
+                    <span class="image featured">
+                      <img src={pic01} alt="" />
+                    </span>
+                    <div class="inner">
+                      <header>
+                        <h2>Put something here</h2>
+                        <p>Maybe here as well I think</p>
+                      </header>
+                      <p>
+                        Phasellus quam turpis, feugiat sit amet in, hendrerit in
+                        lectus. Praesent sed semper amet bibendum tristique
+                        fringilla.
+                      </p>
+                    </div>
+                  </section>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
